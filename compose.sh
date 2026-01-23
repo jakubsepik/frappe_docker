90d95cd3ea3beb25b576de85404c484a51fe7a91
Jakub Šepeľa <jakubsepik@gmail.com>
2026-01-23 13:44:46 +0100

    change to https

--- compose.sh
+++ compose.sh
@@ -1,1 +1,1 @@
-docker compose -p frappe -f compose.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml -f overrides/compose.noproxy.yaml config > compose.my-custom.yaml
+docker compose -p frappe -f compose.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml -f overrides/compose.https.yaml config > compose.my-custom.yaml
