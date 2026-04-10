ce4ada4198ad5fd8298ea24d85fccbf6e9e238f5
Jakub Šepeľa <jakubsepik@gmail.com>
2026-04-10 15:53:03 +0200

    erpnext update

--- build_custom_image.sh
+++ build_custom_image.sh
@@ -1,7 +1,5 @@
 export APPS_JSON_BASE64=$(base64 -w 0 apps.json)
 docker build \
- --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
- --build-arg=FRAPPE_BRANCH=version-15 \
- --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
- --tag=custom:15 \
- --file=images/layered/Containerfile .
+  --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
+  --tag=custom:16 \
+  --file=images/layered/Containerfile .
